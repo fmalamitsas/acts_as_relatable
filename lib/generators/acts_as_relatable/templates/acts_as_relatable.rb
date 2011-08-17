e28e1e2800b1ab5a1be2e28e479b5308a1fce9db
Frédéric Malamitsas <f.malamitsas@gmail.com>
2011-08-17 10:25:14 +0200

    Relationship shortcut option.

--- lib/generators/acts_as_relatable/templates/acts_as_relatable.rb
+++ lib/generators/acts_as_relatable/templates/acts_as_relatable.rb
@@ -1,2 +1,5 @@
 # Uncomment the following line if you want to add methods to the ActsAsRelatable::Relationship AR model
 #ActsAsRelatable::Relationship.send(:include, RelationshipExtension)
+
+#Uncomment this line if you want a shortcut for the Relationship class
+#Relationship = ActsAsRelatable::Relationship
